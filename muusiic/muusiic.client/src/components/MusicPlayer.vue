44647d7aa3c398a57e04c9b08d308cc8d90c7dcd
ThomF <seniorswanky@icloud.com>
2023-08-19 12:53:23 -0600

    switched to local mp3

--- muusiic/muusiic.client/src/components/MusicPlayer.vue
+++ muusiic/muusiic.client/src/components/MusicPlayer.vue
@@ -3,8 +3,11 @@
       <div class="row">
         <div class="col-4">
           <form @submit.prevent="loadAudio">
-            <input type="text" placeholder="MUSIC URL" v-model="audioUrl">
-            <button class="btn btn-success" type="submit"><i class="mdi mdi-music"></i></button>
+            <select v-model="selectedAudio" @change="updateAudioUrl">
+              <option value="">Select an MP3</option>
+              <option v-for="mp3 in mp3Options" :key="mp3.value" :value="mp3.value">{{ mp3.label }}</option>
+            </select>
+            <button class="btn btn-success" type="submit" :disabled="selectedAudio === ''"><i class="mdi mdi-music"></i></button>
           </form>
         </div>
         <div class="col-4">
@@ -14,6 +17,7 @@
           </div>
         </div>
       </div>
+      <div id="youtube-player"></div>
     </div>
   </template>
   
@@ -21,16 +25,23 @@
   import { ref } from "vue";
   import AudioVisualizer from "../components/AudioVisualizer.vue";
   
+//   const audioFile = ('@/assets/your-audio-file.mp3'); // Import the MP3 file
+  
   export default {
     components: {
       AudioVisualizer,
     },
     data() {
       return {
-        audioUrl: "",
+        selectedAudio: "", // Selected audio file
+        audioUrl: "", // No need to initialize with a URL
         audioContext: null,
         sourceNode: null,
         userInteracted: false,
+        mp3Options: [ // List of MP3 options
+          { label: "DaftFrancis", value: "../src/assets/img/Daft Punk - Harder, Better, Faster, Stronger (Dillon Francis Remix).mp3" },
+          // Add more options as needed
+        ],
       };
     },
     methods: {
@@ -44,13 +55,16 @@
         // Create the audio context
         this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
   
-        // Load the audio
-        this.$refs.audio.src = this.audioUrl;
-        this.$refs.audio.addEventListener("loadedmetadata", () => {
-          // Create the source node and connect it to the audio context
-          this.sourceNode = this.audioContext.createMediaElementSource(this.$refs.audio);
-          this.sourceNode.connect(this.audioContext.destination);
-        });
+        // Set the source to the selected audio file
+        this.$refs.audio.src = this.selectedAudio;
+  
+        // Create the source node and connect it to the audio context
+        this.sourceNode = this.audioContext.createMediaElementSource(this.$refs.audio);
+        this.sourceNode.connect(this.audioContext.destination);
+      },
+      updateAudioUrl() {
+        // Update the audio URL when the selected option changes
+        this.audioUrl = this.selectedAudio;
       },
     },
   };
